def3458ead8d2728e304083412546825dfc6e5c5
Simon Wang <mtwangyugeng@gmail.com>
2022-08-23 10:47:09 -0400

    added shake api to control window shakes

--- client/src/assets/Components/DropdownWindow.svelte
+++ client/src/assets/Components/DropdownWindow.svelte
@@ -7,11 +7,13 @@
         }
     export let title = 'Add Title'
 
+    export let isShaking = false;
+    $: if(isShaking) setTimeout(() => isShaking = false, 500)
 </script>
 
 <section>
-    <div class=WindowContainer>
-        <div class=Window>
+    <div class=WindowContainer >
+        <div class=Window class:Shake={isShaking}>
             <div class=TopBar>
                 <div class=Title>
                     {title}
@@ -23,6 +25,7 @@
             <div class=Content>
                 <slot />
             </div>
+        </div>
     </div>
 
 </section>
@@ -88,5 +91,26 @@
         width: 20px;
         margin-right: 5px;
         cursor: pointer;
+    }
+
+
+    .Shake {
+        animation: shake 0.5s;
+        /* When the animation is finished, start again */
+        animation-iteration-count: 1; 
+    }
+
+    @keyframes shake {
+        0% { transform: translate(1px, 1px) rotate(0deg); }
+        10% { transform: translate(-1px, -2px) rotate(-1deg); }
+        20% { transform: translate(-3px, 0px) rotate(1deg); }
+        30% { transform: translate(3px, 2px) rotate(0deg); }
+        40% { transform: translate(1px, -1px) rotate(1deg); }
+        50% { transform: translate(-1px, 2px) rotate(-1deg); }
+        60% { transform: translate(-3px, 1px) rotate(0deg); }
+        70% { transform: translate(3px, 1px) rotate(-1deg); }
+        80% { transform: translate(-1px, -1px) rotate(1deg); }
+        90% { transform: translate(1px, 2px) rotate(0deg); }
+        100% { transform: translate(1px, -2px) rotate(-1deg); }
     }
 </style>
