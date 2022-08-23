97e20c54ee1e08dd442d97f5bd9d8422a8d80e71
Simon Wang <mtwangyugeng@gmail.com>
2022-08-23 10:59:00 -0400

    moved style initialization into a reusable file

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,26 +1,9 @@
 <script>
+import IniStyle from './assets/Components/_IniStyle.svelte';
 import SandBox from './lib/SandBox.svelte'
 </script>
 
 <main>
   <SandBox />
+  <IniStyle />
 </main>
-
-<style>
-
-  :global(*) {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-  }
-
-  :global(button), :global(input[type="submit"]), :global(input[type="reset"]) {
-		background: none;
-		color: inherit;
-		border: none;
-		padding: 0;
-		font: inherit;
-		cursor: pointer;
-		outline: inherit;
-	}
-</style>
---
+++ client/src/assets/Components/_IniStyle.svelte
@@ -0,0 +1,18 @@
+<style>
+    :global(*) {
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+    }
+  
+    :global(button), :global(input[type="submit"]), :global(input[type="reset"]) {
+        background: none;
+        color: inherit;
+        border: none;
+        padding: 0;
+        font: inherit;
+        cursor: pointer;
+        outline: inherit;
+    }
+</style>
+  
