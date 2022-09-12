a485a29e8a1853ac5705453b6f2f558b5de2322d
Simon Wang <mtwangyugeng@gmail.com>
2022-09-12 10:18:15 -0400

    fixed: Login does not show loading screen

--- client/src/lib/DropDowns/Login.svelte
+++ client/src/lib/DropDowns/Login.svelte
@@ -36,6 +36,7 @@
     }
 
     async function handleSubmit() {
+        isLoading = true;
         let status = null;
 
         if (isCreatingAccount) {
@@ -47,6 +48,7 @@
             handleClose()
         }
 
+        isLoading = false;
         return status
     }
 </script>
