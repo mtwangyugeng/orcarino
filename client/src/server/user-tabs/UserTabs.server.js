9b902a19bb271472c47276efcffb35c679d663fb
Simon Wang <mtwangyugeng@gmail.com>
2022-09-15 23:38:33 -0400

    serverside mockup for user tabs and community pag

---
+++ client/src/server/user-tabs/UserTabs.server.js
@@ -0,0 +1,23 @@
+import { innerGetCommunityById } from "../community/Community.server";
+import { findInCsvArray } from "../_server";
+
+const userTabsData = [
+    ['id', 'tabIds'], 
+    ['qweWQDAS21DAqww', '1;3;5'],
+    ['2134134asdSADWED', '2;4'],
+    ];
+
+
+export async function serverGetUserTabs(id) {
+    const index = findInCsvArray(userTabsData, 'id', id)
+    if (index < 0) return {success:false, err: "User id does not exist."};
+
+    const tabsString = userTabsData[index][1];
+    const tabArr = tabsString.split(';');
+
+    const finale = tabArr.map(tabId => {
+        return innerGetCommunityById(tabId);
+    });
+
+    return {success:true, userTabs: finale}
+}
